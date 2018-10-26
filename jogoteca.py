8145fc4ab22c9c5c22ee2d7406db5826d7501a1d
rodrigocromai <rodrigo.santos@cromai.com>
2018-10-26 16:52:58 -0200

    mais instrucoes para autenticacao

--- jogoteca.py
+++ jogoteca.py
@@ -16,33 +16,39 @@ lista = []
 
 @app.route('/jogos')
 def index():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/')
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/?proxima=novo')
     return render_template('novo.html', titulo='Novo Jogo')
 
 
 @app.route('/criar', methods=['POST',])
 def criar():
-    nome = request. form['nome']
-    categoria = request. form['categoria']
-    console = request. form['console']
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
     return redirect('/jogos')
 
 @app.route('/')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html',proxima=proxima)
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
-    if 'mestra' == request.form['senha']:
+    if '123' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
         flash(request.form['usuario'] + ' logou com sucesso!')
-        return redirect('/jogos')
+        proximaPagina = request.form['proxima']
+        return redirect('/{}'.format(proximaPagina))
     else:
         flash('Não logado, tente novamente!')
         return redirect('/')
@@ -54,7 +60,9 @@ def logout():
     return redirect('/')
 
 
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("/index.html")
 
 
 app.run(debug=True)
